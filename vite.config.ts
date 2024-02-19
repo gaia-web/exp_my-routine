e0e8ed897dca41c8637ccccbe7616a12e2b42f94
Sean Wong <seanwong24@live.com>
2024-02-18 19:55:27 -0800

    setup PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,38 @@
-import legacy from '@vitejs/plugin-legacy'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    legacy()
+    legacy(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifest: {
+        name: "My Routine",
+        short_name: "MyRoutine",
+        start_url: "/",
+        icons: [
+          {
+            src: "./icon.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
   ],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
   test: {
     globals: true,
-    environment: 'jsdom'
-  }
-})
+    environment: "jsdom",
+  },
+});
