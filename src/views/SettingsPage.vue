b695c0b6fc86b13a201538caef09508888f7f813
willwei954 <41525364+willwei954@users.noreply.github.com>
2024-02-19 02:22:28 -0500

    Setting page (#1)

--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -12,12 +12,36 @@
         </ion-toolbar>
       </ion-header>
 
-      <ExploreContainer name="Settings page" />
+      <ion-list>
+        <ion-item button>
+          <ion-icon name="person-circle-outline-outline" src="/resources/person-circle-outline.svg"
+            slot="start"></ion-icon>
+          <ion-label>
+            Account
+          </ion-label>
+        </ion-item>
+        <ion-item>
+          <ion-icon name="contrast-outline" src="/resources/contrast-outline.svg" slot="start"></ion-icon>
+          <ion-toggle>
+            Darkmode
+          </ion-toggle>
+        </ion-item>
+        <ion-item >
+          <ion-icon name="hammer-outline" src="/resources/hammer-outline.svg" slot="start"></ion-icon>
+          <ion-label>
+            Version
+          </ion-label>
+          <ion-note color="medium" class="ion-text-wrap">1.0.0</ion-note>
+        </ion-item>
+        <ion-item button lines="none">
+          <ion-icon name="settingsx-outline" src="/resources/settings-outline.svg" slot="start"></ion-icon>
+          General
+        </ion-item>
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
 </script>
