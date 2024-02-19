e6147213898921cf1f95122e3f2bdbb62136722c
Sean Wong <seanwong24@live.com>
2024-02-19 07:34:49 +0000

    fix assets

--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -14,27 +14,26 @@
 
       <ion-list>
         <ion-item button>
-          <ion-icon name="person-circle-outline-outline" src="/resources/person-circle-outline.svg"
-            slot="start"></ion-icon>
+          <ion-icon slot="start" :icon="personCircleOutline"></ion-icon>
           <ion-label>
             Account
           </ion-label>
         </ion-item>
         <ion-item>
-          <ion-icon name="contrast-outline" src="/resources/contrast-outline.svg" slot="start"></ion-icon>
+          <ion-icon slot="start" :icon="contrastOutline"></ion-icon>
           <ion-toggle>
             Darkmode
           </ion-toggle>
         </ion-item>
-        <ion-item >
-          <ion-icon name="hammer-outline" src="/resources/hammer-outline.svg" slot="start"></ion-icon>
+        <ion-item>
+          <ion-icon slot="start" :icon="hammerOutline"></ion-icon>
           <ion-label>
             Version
           </ion-label>
           <ion-note color="medium" class="ion-text-wrap">1.0.0</ion-note>
         </ion-item>
         <ion-item button lines="none">
-          <ion-icon name="settingsx-outline" src="/resources/settings-outline.svg" slot="start"></ion-icon>
+          <ion-icon slot="start" :icon="settingsOutline"></ion-icon>
           General
         </ion-item>
       </ion-list>
@@ -44,4 +43,5 @@
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import { contrastOutline, hammerOutline, personCircleOutline, settingsOutline } from 'ionicons/icons';
 </script>
