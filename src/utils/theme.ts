0897f97979e7ec4190469e9aaf80c6cb503b8946
willwei954 <41525364+willwei954@users.noreply.github.com>
2024-02-20 22:09:36 -0500

    Add Theme change functionality in Setting page (#3)
    * select so far
    
    * Add functionality to change theme
    
    * fixes and refactors for theme (#4)
    
    * fix issues and refactor
    
    * also update color scheme of the document
    
    * fix theme initialization
    
    * format
    
    * remove unnecessary code
    
    * default value
    
    * rm
    
    * use ??
    
    ---------
    
    Co-authored-by: Sean Wong <seanwong24@live.com>

---
+++ src/utils/theme.ts
@@ -0,0 +1,17 @@
+import { STORAGE_KEYS } from "./constant";
+
+const mediaPrefersDarkScheme = window.matchMedia(
+  "(prefers-color-scheme: dark)"
+);
+mediaPrefersDarkScheme.addEventListener("change", () => updateTheme());
+
+export const updateTheme = () => {
+  const currentThemeType =
+    localStorage.getItem(STORAGE_KEYS.THEME_TYPE) ?? "system";
+  let isDark = mediaPrefersDarkScheme.matches;
+  if (currentThemeType !== "system") {
+    isDark = currentThemeType === "dark";
+  }
+  document.body.classList.toggle("dark", isDark);
+  document.documentElement.style.colorScheme = isDark ? "dark" : "light";
+};
--- src/views/SettingsPage.vue
+++ src/views/SettingsPage.vue
@@ -15,22 +15,26 @@
       <ion-list>
         <ion-item button>
           <ion-icon slot="start" :icon="personCircleOutline"></ion-icon>
-          <ion-label>
-            Account
-          </ion-label>
+          <ion-label> Account </ion-label>
         </ion-item>
-        <ion-item>
+        <ion-item width="full">
           <ion-icon slot="start" :icon="contrastOutline"></ion-icon>
-          <ion-toggle>
-            Darkmode
-          </ion-toggle>
+
+          <ion-select
+            label="Color Theme"
+            aria-label="theme"
+            interface="popover"
+            v-model="themeType"
+          >
+            <ion-select-option value="system">System</ion-select-option>
+            <ion-select-option value="dark">Dark</ion-select-option>
+            <ion-select-option value="light">Light</ion-select-option>
+          </ion-select>
         </ion-item>
         <ion-item>
           <ion-icon slot="start" :icon="hammerOutline"></ion-icon>
-          <ion-label>
-            Version
-          </ion-label>
-          <ion-note color="medium" class="ion-text-wrap">1.0.0</ion-note>
+          <ion-label> Version </ion-label>
+          <ion-note slot="end" color="medium">1.0.0</ion-note>
         </ion-item>
         <ion-item button lines="none">
           <ion-icon slot="start" :icon="settingsOutline"></ion-icon>
@@ -42,6 +46,35 @@
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonToggle } from '@ionic/vue';
-import { contrastOutline, hammerOutline, personCircleOutline, settingsOutline } from 'ionicons/icons';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonSelect,
+  IonLabel,
+  IonItem,
+  IonSelectOption,
+  IonIcon,
+  IonNote,
+  IonList,
+} from "@ionic/vue";
+import {
+  contrastOutline,
+  hammerOutline,
+  personCircleOutline,
+  settingsOutline,
+} from "ionicons/icons";
+import { onMounted, ref, watch } from "vue";
+import { STORAGE_KEYS } from "../utils/constant";
+import { updateTheme } from "../utils/theme";
+
+const themeType = ref(
+  localStorage.getItem(STORAGE_KEYS.THEME_TYPE) ?? "system"
+);
+watch(themeType, () => {
+  localStorage.setItem(STORAGE_KEYS.THEME_TYPE, themeType.value);
+  updateTheme();
+});
 </script>
