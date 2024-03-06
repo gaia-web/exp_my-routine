c57c0fbc422d4f9f1e0c290118ceb6931e2f3d6e
Sean Wong <seanwong24@live.com>
2024-03-05 22:14:52 -0600

    initialize app data (#8)

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,5 +1,8 @@
 import { Storage } from "@ionic/storage";
+import { initializeAppData } from "./app-data";
 
 export const appStorage = await new Storage({
   name: "app-storage",
 }).create();
+
+initializeAppData();
