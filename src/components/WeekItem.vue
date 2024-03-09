fe27f6fdac14e6b544bdbb4d23e4c32258e84c9e
Sean Wong <seanwong24@live.com>
2024-03-08 20:15:22 -0800

    clean up

--- src/components/WeekItem.vue
+++ src/components/WeekItem.vue
@@ -1,21 +1,19 @@
 <template>
-  <!-- <ion-item> -->
-    <div style="width: 100%">
-      <div>{{ header }}</div>
-      <div style="display: flex">
-        <ion-button
-          v-for="(_, i) in Array(7)"
-          :key="i"
-          fill="outline"
-          style="flex: 1"
-        ></ion-button>
-      </div>
+  <div style="width: 100%">
+    <div>{{ header }}</div>
+    <div style="display: flex">
+      <ion-button
+        v-for="(_, i) in Array(7)"
+        :key="i"
+        fill="outline"
+        style="flex: 1"
+      ></ion-button>
     </div>
-  <!-- </ion-item> -->
+  </div>
 </template>
 
 <script setup lang="ts">
-import { IonButton, IonItem } from "@ionic/vue";
+import { IonButton } from "@ionic/vue";
 defineProps({
   header: String,
 });
